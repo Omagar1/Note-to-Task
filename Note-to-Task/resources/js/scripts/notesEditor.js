28cbac043229a6822fa3aefd9fb5b9a3fd394783
Omagar1 <josiah.rowden@gmail.com>
2026-04-14 12:21:48 +0100

    some more progress
    <template x-for> now used for tasks loop

--- Note-to-Task/resources/js/scripts/notesEditor.js
+++ Note-to-Task/resources/js/scripts/notesEditor.js
@@ -1,3 +1,4 @@
+import { stringify } from "postcss";
 
 
 export default function noteEditor({ initialContent, noteId, route, csrfToken} ) {
@@ -75,18 +76,29 @@ export default function noteEditor({ initialContent, noteId, route, csrfToken} )
 
                     // show on front end
                     
-                    // task format 
-                    // <label for="checkboxSuccess" class="flex items-center gap-2 text-sm font-medium text-on-surface dark:text-on-surface-dark has-checked:text-on-surface-strong dark:has-checked:text-on-surface-dark-strong has-disabled:opacity-75 has-disabled:cursor-not-allowed">
-                    //     <span> </span>
-                    //     <span class="relative flex items-center">
-                    //         <input id="checkboxSuccess" type="checkbox" class="after:content-[''] peer relative size-4 appearance-none overflow-hidden rounded-sm border border-outline bg-surface-alt before:absolute before:inset-0 checked:border-success checked:before:bg-success focus:outline-2 focus:outline-offset-2 focus:outline-outline-strong checked:focus:outline-success active:outline-offset-0 disabled:cursor-not-allowed dark:border-outline-dark dark:bg-surface-dark-alt dark:checked:border-success dark:checked:before:bg-success dark:focus:outline-outline-dark-strong dark:checked:focus:outline-success" checked />
-                    //         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" stroke="currentColor" fill="none" stroke-width="4" class="pointer-events-none invisible absolute left-1/2 top-1/2 size-3 -translate-x-1/2 -translate-y-1/2 text-on-success peer-checked:visible dark:text-on-success-dark">
-                    //             <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"/>
-                    //         </svg>
-                    //     </span>
-                    // </label>
+                    let taskContainer = document.getElementById("taskContainer");
+                    let oldXData = taskContainer.getAttribute('x-data').tasks;
+                    console.log("oldXData: ", oldXData);
+                    let newXData = {title: taskTitle };// add other attributes as necessary
+
+                    if(oldXData){
+                        newXData = `{tasks:  ${oldXData['tasks'].append(newXData)} }`;
+                    } else{
+                        newXData = `{tasks: ${newXData} }`;
+                    }
+
+                    console.log("newXData: ", newXData);
                     
-                    $refs.noTaskMsg.remove()
+                    taskContainer.setAttribute('x-data', newXData );
+
+                    //test
+                    let testTaskContainer = document.getElementById("taskContainer");
+                    let testXData = testTaskContainer.getAttribute('x-data').tasks;
+                    console.log("testXData: ", testXData);
+
+                    
+                    
+                    
 
 
                 }                
